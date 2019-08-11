2442a4853808cdb42eab96a975c39d18023df446
kwan0039 <kevin.wang@monash.edu>
2019-08-12 08:24:31 +1000

    Firt Submit

---
+++ WebApplication9/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication9.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            HelloWorld.Hello hello = new HelloWorld.Hello();
+            ViewBag.Message = hello.GetHello();
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
